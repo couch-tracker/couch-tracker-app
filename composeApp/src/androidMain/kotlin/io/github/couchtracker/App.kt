e37033e45d294932f112e5c6099733d279e0e679
Mauro Molin <molin.mauro@gmail.com>
2024-02-08 21:46:22 +0000

    Move App to main package

--- composeApp/src/androidMain/kotlin/io/github/couchtracker/App.kt
+++ composeApp/src/androidMain/kotlin/io/github/couchtracker/App.kt
@@ -1,3 +1,5 @@
+package io.github.couchtracker
+
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
