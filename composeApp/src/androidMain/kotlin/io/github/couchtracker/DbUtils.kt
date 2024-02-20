d1319a693b7339e2e2241e170033f7c71b5e8655
Mauro Molin <molin.mauro@gmail.com>
2024-02-20 23:38:24 +0000

    Fix detekt warnings

--- composeApp/src/androidMain/kotlin/io/github/couchtracker/DbUtils.kt
+++ composeApp/src/androidMain/kotlin/io/github/couchtracker/DbUtils.kt
@@ -7,8 +7,9 @@ import app.cash.sqldelight.Query
 import app.cash.sqldelight.coroutines.asFlow
 import app.cash.sqldelight.coroutines.mapToList
 import kotlinx.coroutines.Dispatchers
+import kotlin.coroutines.CoroutineContext
 
 @Composable
-fun <T : Any> Query<T>.asListState(): State<List<T>?> {
-    return this.asFlow().mapToList(Dispatchers.IO).collectAsStateWithLifecycle(null)
+fun <T : Any> Query<T>.asListState(coroutineContext: CoroutineContext = Dispatchers.IO): State<List<T>?> {
+    return this.asFlow().mapToList(coroutineContext).collectAsStateWithLifecycle(null)
 }
