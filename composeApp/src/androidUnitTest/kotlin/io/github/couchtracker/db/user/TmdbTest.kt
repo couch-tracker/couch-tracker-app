2199253c8c33c0887eab672d03209c0cc6bb076f
Mauro Molin <molin.mauro@gmail.com>
2024-02-21 00:24:07 +0000

    Fix Detekt issues

--- composeApp/src/androidUnitTest/kotlin/io/github/couchtracker/db/user/TmdbTest.kt
+++ composeApp/src/androidUnitTest/kotlin/io/github/couchtracker/db/user/TmdbTest.kt
@@ -9,14 +9,14 @@ class TmdbTest : FunSpec(
     {
         context("requireTmdbId()") {
             context("works with valid IDs") {
-                withData(listOf(1, 50, 1200, 999_999, Long.MAX_VALUE)) {id ->
+                withData(listOf(1, 50, 1200, 999_999, Long.MAX_VALUE)) { id ->
                     assertDoesNotThrow {
                         requireTmdbId(id)
                     }
                 }
             }
             context("fails with invalid IDs") {
-                withData(0, -1, -1234, Long.MIN_VALUE) {id ->
+                withData(0, -1, -1234, Long.MIN_VALUE) { id ->
                     assertThrows<IllegalArgumentException> {
                         requireTmdbId(id)
                     }
