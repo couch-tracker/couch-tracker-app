4ad238642b6dccb7a687fbd2d20ac509b3180b8b
Mauro Molin <molin.mauro@gmail.com>
2024-02-23 21:03:03 +0000

    Add documentation to ExternalId and related classes
    Adds explanation on how what the various classes are and how they interact with each other.

--- composeApp/src/androidMain/kotlin/io/github/couchtracker/db/user/Tmdb.kt
+++ composeApp/src/androidMain/kotlin/io/github/couchtracker/db/user/Tmdb.kt
@@ -1,5 +1,8 @@
 package io.github.couchtracker.db.user
 
+/**
+ * Throws [IllegalArgumentException] when the given [id] is not a valid TMDB ID.
+ */
 fun requireTmdbId(id: Long) {
     require(id > 0) { "Invalid non-positive TMDB id: $id" }
 }
