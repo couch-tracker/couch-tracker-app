2c95da705a1794d5f35cbd107bb866a5be4f7b50
Mauro Molin <molin.mauro@gmail.com>
2024-03-24 16:00:49 +0000

    Change User table for external DB support

--- composeApp/src/androidMain/kotlin/io/github/couchtracker/db/common/Adapters.kt
+++ composeApp/src/androidMain/kotlin/io/github/couchtracker/db/common/Adapters.kt
@@ -1,8 +1,20 @@
 package io.github.couchtracker.db.common
 
+import android.net.Uri
 import app.cash.sqldelight.ColumnAdapter
+import kotlinx.datetime.Instant
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
+
+object InstantColumnAdapter : ColumnAdapter<Instant, String> {
+    override fun encode(value: Instant) = value.toString()
+    override fun decode(databaseValue: String) = Instant.parse(databaseValue)
+}
+
+object UriColumnAdapter : ColumnAdapter<Uri, String> {
+    override fun encode(value: Uri) = value.toString()
+    override fun decode(databaseValue: String): Uri = Uri.parse(databaseValue)
+}
 
 inline fun <reified T : Any> jsonAdapter() = object : ColumnAdapter<T, String> {
     override fun decode(databaseValue: String) = Json.decodeFromString<T>(databaseValue)
