ac7563f1ef7510b20da3b035efeb5b7742c7f174
Mauro Molin <molin.mauro@gmail.com>
2024-07-23 23:18:39 +0100

    Add WatchedMovie table

--- composeApp/src/androidMain/kotlin/io/github/couchtracker/db/user/UserDbModule.kt
+++ composeApp/src/androidMain/kotlin/io/github/couchtracker/db/user/UserDbModule.kt
@@ -2,6 +2,10 @@ package io.github.couchtracker.db.user
 
 import io.github.couchtracker.db.common.AndroidSqliteDriverFactory
 import io.github.couchtracker.db.common.SqliteDriverFactory
+import io.github.couchtracker.db.common.adapters.InstantColumnAdapter
+import io.github.couchtracker.db.common.adapters.PartialDateTimeColumnAdapter
+import io.github.couchtracker.db.user.movie.ExternalMovieId
+import io.github.couchtracker.db.user.show.ExternalShowId
 import org.koin.core.qualifier.named
 import org.koin.dsl.bind
 import org.koin.dsl.module
@@ -10,4 +14,19 @@ val UserDbModule = module {
     factory(named("UserDb")) {
         AndroidSqliteDriverFactory(schema = UserData.Schema)
     }.bind<SqliteDriverFactory>()
+
+    factory<UserData> { params ->
+        UserData(
+            driver = params.get(),
+            ShowInCollectionAdapter = ShowInCollection.Adapter(
+                showIdAdapter = ExternalShowId.columnAdapter(),
+                addDateAdapter = InstantColumnAdapter,
+            ),
+            WatchedMovieAdapter = WatchedMovie.Adapter(
+                movieIdAdapter = ExternalMovieId.columnAdapter(),
+                addedAtAdapter = InstantColumnAdapter,
+                watchedAtAdapter = PartialDateTimeColumnAdapter,
+            ),
+        )
+    }
 }
