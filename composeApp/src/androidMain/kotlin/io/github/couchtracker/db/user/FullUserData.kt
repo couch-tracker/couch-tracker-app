4a6c08e4dded35c54ebf619b8f27a721e9dc0b54
Mauro Molin <molin.mauro@gmail.com>
2024-08-19 22:08:31 +0100

    Add watched movies to FullUserData

--- composeApp/src/androidMain/kotlin/io/github/couchtracker/db/user/FullUserData.kt
+++ composeApp/src/androidMain/kotlin/io/github/couchtracker/db/user/FullUserData.kt
@@ -12,6 +12,7 @@ private const val LOG_TAG = "FullUserData"
 
 data class FullUserData(
     val showCollection: List<ShowInCollection>,
+    val watchedMovies: List<WatchedMovie>,
 ) {
 
     companion object {
@@ -21,6 +22,7 @@ data class FullUserData(
             val (data, time) = measureTimedValue {
                 FullUserData(
                     showCollection = db.showInCollectionQueries.selectShowCollection().asFlow().mapToList(coroutineContext).first(),
+                    watchedMovies = db.watchedMovieQueries.selectAll().asFlow().mapToList(coroutineContext).first(),
                 )
             }
             Log.d(LOG_TAG, "Loading full user data took $time")
