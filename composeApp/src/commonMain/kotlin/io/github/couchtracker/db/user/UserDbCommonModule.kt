ed7fda506e011b7d7f3d5377765ce13af5db325e
Mauro Molin <molin.mauro@gmail.com>
2024-08-31 01:06:53 +0100

    Add WatchedItemDimension and related tables
    For now only the `choice` and `freeText` types are supported.
    
    Dimensions can be enabled conditionally based on the selection status of another dimension choice.

--- composeApp/src/commonMain/kotlin/io/github/couchtracker/db/user/UserDbCommonModule.kt
+++ composeApp/src/commonMain/kotlin/io/github/couchtracker/db/user/UserDbCommonModule.kt
@@ -1,9 +1,16 @@
 package io.github.couchtracker.db.user
 
+import app.cash.sqldelight.EnumColumnAdapter
+import io.github.couchtracker.db.common.adapters.DbIconColumnAdapter
+import io.github.couchtracker.db.common.adapters.DbTextColumnAdapter
 import io.github.couchtracker.db.common.adapters.InstantColumnAdapter
 import io.github.couchtracker.db.common.adapters.PartialDateTimeColumnAdapter
 import io.github.couchtracker.db.common.adapters.WatchableExternalIdColumnAdapter
 import io.github.couchtracker.db.common.adapters.columnAdapter
+import io.github.couchtracker.db.common.adapters.jsonAdapter
+import io.github.couchtracker.db.common.adapters.jsonSet
+import io.github.couchtracker.db.user.model.watchedItem.WatchedItemDimensionType
+import io.github.couchtracker.db.user.model.watchedItem.WatchedItemType
 import io.github.couchtracker.db.user.show.ExternalShowId
 import org.koin.dsl.module
 
@@ -19,6 +26,15 @@ val UserDbCommonModule = module {
                 itemIdAdapter = WatchableExternalIdColumnAdapter,
                 addedAtAdapter = InstantColumnAdapter,
                 watchAtAdapter = PartialDateTimeColumnAdapter,
+            ),
+            WatchedItemDimensionAdapter = WatchedItemDimension.Adapter(
+                appliesToAdapter = EnumColumnAdapter<WatchedItemType>().jsonSet(),
+                nameAdapter = DbTextColumnAdapter,
+                typeAdapter = jsonAdapter<WatchedItemDimensionType>(),
+            ),
+            WatchedItemDimensionChoiceAdapter = WatchedItemDimensionChoice.Adapter(
+                nameAdapter = DbTextColumnAdapter,
+                iconAdapter = DbIconColumnAdapter,
             ),
         )
     }
