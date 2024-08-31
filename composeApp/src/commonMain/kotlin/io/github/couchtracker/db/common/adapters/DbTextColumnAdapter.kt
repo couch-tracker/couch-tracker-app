9340112f8bdf96fb92438fb6cb66abb51032f9bd
Mauro Molin <molin.mauro@gmail.com>
2024-08-31 13:52:48 +0100

    Add DbTextColumnAdapter

---
+++ composeApp/src/commonMain/kotlin/io/github/couchtracker/db/common/adapters/DbTextColumnAdapter.kt
@@ -0,0 +1,9 @@
+package io.github.couchtracker.db.common.adapters
+
+import app.cash.sqldelight.ColumnAdapter
+import io.github.couchtracker.db.user.model.text.DbText
+
+val DbTextColumnAdapter: ColumnAdapter<DbText, String> = CouchTrackerUriColumnAdapter.map(
+    encoder = { it.toCouchTrackerUri() },
+    decoder = { DbText.fromUri(it) },
+)
