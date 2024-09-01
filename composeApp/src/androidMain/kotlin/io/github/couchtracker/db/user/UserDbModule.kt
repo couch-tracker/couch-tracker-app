05e1d1531dcd98988a4aa96eff06fb63519a65a5
Mauro Molin <molin.mauro@gmail.com>
2024-09-01 01:42:24 +0100

    Move UserDbModule to common module

--- composeApp/src/androidMain/kotlin/io/github/couchtracker/db/user/UserDbModule.kt
+++ composeApp/src/androidMain/kotlin/io/github/couchtracker/db/user/UserDbModule.kt
@@ -2,31 +2,14 @@ package io.github.couchtracker.db.user
 
 import io.github.couchtracker.db.common.AndroidSqliteDriverFactory
 import io.github.couchtracker.db.common.SqliteDriverFactory
-import io.github.couchtracker.db.common.adapters.InstantColumnAdapter
-import io.github.couchtracker.db.common.adapters.PartialDateTimeColumnAdapter
-import io.github.couchtracker.db.user.movie.ExternalMovieId
-import io.github.couchtracker.db.user.show.ExternalShowId
 import org.koin.core.qualifier.named
 import org.koin.dsl.bind
 import org.koin.dsl.module
 
 val UserDbModule = module {
+    includes(UserDbCommonModule)
+
     factory(named("UserDb")) {
         AndroidSqliteDriverFactory(schema = UserData.Schema)
     }.bind<SqliteDriverFactory>()
-
-    factory<UserData> { params ->
-        UserData(
-            driver = params.get(),
-            ShowInCollectionAdapter = ShowInCollection.Adapter(
-                showIdAdapter = ExternalShowId.columnAdapter(),
-                addDateAdapter = InstantColumnAdapter,
-            ),
-            WatchedMovieAdapter = WatchedMovie.Adapter(
-                movieIdAdapter = ExternalMovieId.columnAdapter(),
-                addedAtAdapter = InstantColumnAdapter,
-                watchedAtAdapter = PartialDateTimeColumnAdapter,
-            ),
-        )
-    }
 }
---
+++ composeApp/src/commonMain/kotlin/io/github/couchtracker/db/user/UserDbCommonModule.kt
@@ -0,0 +1,24 @@
+package io.github.couchtracker.db.user
+
+import io.github.couchtracker.db.common.adapters.InstantColumnAdapter
+import io.github.couchtracker.db.common.adapters.PartialDateTimeColumnAdapter
+import io.github.couchtracker.db.user.movie.ExternalMovieId
+import io.github.couchtracker.db.user.show.ExternalShowId
+import org.koin.dsl.module
+
+val UserDbCommonModule = module {
+    factory<UserData> { params ->
+        UserData(
+            driver = params.get(),
+            ShowInCollectionAdapter = ShowInCollection.Adapter(
+                showIdAdapter = ExternalShowId.columnAdapter(),
+                addDateAdapter = InstantColumnAdapter,
+            ),
+            WatchedMovieAdapter = WatchedMovie.Adapter(
+                movieIdAdapter = ExternalMovieId.columnAdapter(),
+                addedAtAdapter = InstantColumnAdapter,
+                watchedAtAdapter = PartialDateTimeColumnAdapter,
+            ),
+        )
+    }
+}
