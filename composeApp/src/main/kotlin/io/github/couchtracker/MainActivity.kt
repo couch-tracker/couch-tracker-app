1c5890638db6a317d32efc12de67eee04f6a996b
Marco <marco.marangoni1@gmail.com>
2025-05-02 11:24:38 +0100

    Force dark status bar
    Fixes #32

--- composeApp/src/main/kotlin/io/github/couchtracker/MainActivity.kt
+++ composeApp/src/main/kotlin/io/github/couchtracker/MainActivity.kt
@@ -14,6 +14,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge(
+            statusBarStyle = SystemBarStyle.dark(Color.Transparent.toArgb()),
             navigationBarStyle = SystemBarStyle.dark(Color.Black.copy(NAVIGATION_BAR_ALPHA).toArgb()),
         )
         setContent {
