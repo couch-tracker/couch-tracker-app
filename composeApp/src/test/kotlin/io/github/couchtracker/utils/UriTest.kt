99e5f0d8e88b3eb76768ba3a70967e7829480c7d
Mauro Molin <molin.mauro@gmail.com>
2025-05-24 17:32:16 +0100

    Fix encodeUriQuery and rename to encodeUriComponent
    The old function was not working as expected, skipping escaping some characters.
    
    As there is no build-in function to to this, we are implementing it ourselves.

--- composeApp/src/test/kotlin/io/github/couchtracker/utils/UriTest.kt
+++ composeApp/src/test/kotlin/io/github/couchtracker/utils/UriTest.kt
@@ -3,20 +3,36 @@ package io.github.couchtracker.utils
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.datatest.withData
 import io.kotest.matchers.shouldBe
+import io.kotest.property.Arb
+import io.kotest.property.arbitrary.string
+import io.kotest.property.checkAll
+import java.net.URI
 
 class UriTest : FunSpec(
     {
-        context("encodeUriQuery()") {
-            withData(
-                "" to "",
-                " " to "%20",
-                "hello" to "hello",
-                "hello world" to "hello%20world",
-                "hello+world" to "hello+world",
-                "hello/world" to "hello/world",
-                "hello#world" to "hello%23world",
-            ) { (decoded, expected) ->
-                encodeUriQuery(decoded) shouldBe expected
+        context("encodeUriComponent()") {
+            context("basic tests") {
+                withData(
+                    "" to "",
+                    " " to "%20",
+                    "hello" to "hello",
+                    "hello-to_everyone.in~the.world" to "hello-to_everyone.in~the.world",
+                    "hello world" to "hello%20world",
+                    "hello+world" to "hello%2Bworld",
+                    "hello/world" to "hello%2Fworld",
+                    "hello#world" to "hello%23world",
+                    "hello=world" to "hello%3Dworld",
+                    "hello&world" to "hello%26world",
+                ) { (decoded, expected) ->
+                    encodeUriComponent(decoded) shouldBe expected
+                }
+            }
+            test("escaped value is correctly parsed by URL") {
+                checkAll(Arb.string()) { query ->
+                    val encoded = encodeUriComponent(query)
+
+                    URI("https://ciao.com?$encoded").query shouldBe query
+                }
             }
         }
     },
