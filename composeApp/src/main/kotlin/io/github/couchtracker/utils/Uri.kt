037eb12d57f3d5ba06f62d0d8ffda5a1ba66d639
Mauro Molin <molin.mauro@gmail.com>
2025-05-24 18:21:18 +0100

    Fix URI.pathSegments
    The previous implementation was wrong because it started from the decoded path, which means that a `%2F` escape sequence in a path segment would be interpreted as an actual `/`, thus wronly plitting the single path segment.

--- composeApp/src/main/kotlin/io/github/couchtracker/utils/Uri.kt
+++ composeApp/src/main/kotlin/io/github/couchtracker/utils/Uri.kt
@@ -19,12 +19,9 @@ fun Uri.toJavaUri(): URI {
 }
 
 fun URI.pathSegments(): List<String> {
-    return path
-        ?.removePrefix("/")
-        ?.removeSuffix("/")
-        ?.takeIf { it.isNotEmpty() }
-        ?.split('/')
-        .orEmpty()
+    return rawPath.split("/")
+        .filter { it.isNotEmpty() }
+        .map { decodeUriComponent(it) }
 }
 
 /**
@@ -39,6 +36,31 @@ fun encodeUriComponent(value: String): String {
             when {
                 char.isLetterOrDigit() || "-_.~".contains(char) -> append(char)
                 else -> append("%" + char.code.toString(radix = 16).uppercase().padStart(2, '0'))
+            }
+        }
+    }
+}
+
+private const val PERCENT_ESCAPE_LENGTH = 3
+
+/**
+ * Decodes the given [value] to be used, as a whole, in a URI query component, compliant with
+ * [RFC 3986](https://datatracker.ietf.org/doc/html/rfc3986).
+ */
+fun decodeUriComponent(value: String): String {
+    // Unfortunately there is no standard way to escape a URI component in the stdlib
+    // URLDecoder.decode decodes `+` as spaces, so that's not RFC 3986 compliant
+    return buildString {
+        var i = 0
+        while (i < value.length) {
+            if (value[i] == '%' && i + 2 < value.length) {
+                val hex = value.substring(i + 1, i + PERCENT_ESCAPE_LENGTH)
+                val decodedChar = hex.toInt(radix = 16).toChar()
+                append(decodedChar)
+                i += PERCENT_ESCAPE_LENGTH
+            } else {
+                append(value[i])
+                i++
             }
         }
     }
