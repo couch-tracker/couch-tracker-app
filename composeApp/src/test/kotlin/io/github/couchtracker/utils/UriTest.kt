d65455af0474b7193275da78c642fc38da8b5aa3
Mauro Molin <molin.mauro@gmail.com>
2025-05-24 18:21:18 +0100

    Fix URI.pathSegments
    The previous implementation was wrong because it started from the decoded path, which means that a `%2F` escape sequence in a path segment would be interpreted as an actual `/`, thus wronly plitting the single path segment.

--- composeApp/src/test/kotlin/io/github/couchtracker/utils/UriTest.kt
+++ composeApp/src/test/kotlin/io/github/couchtracker/utils/UriTest.kt
@@ -1,16 +1,18 @@
 package io.github.couchtracker.utils
 
+import io.kotest.assertions.withClue
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.datatest.withData
 import io.kotest.matchers.shouldBe
 import io.kotest.property.Arb
+import io.kotest.property.arbitrary.list
 import io.kotest.property.arbitrary.string
 import io.kotest.property.checkAll
 import java.net.URI
 
 class UriTest : FunSpec(
     {
-        context("encodeUriComponent()") {
+        context("encode/decode URI component") {
             context("basic tests") {
                 withData(
                     "" to "",
@@ -23,8 +25,13 @@ class UriTest : FunSpec(
                     "hello#world" to "hello%23world",
                     "hello=world" to "hello%3Dworld",
                     "hello&world" to "hello%26world",
-                ) { (decoded, expected) ->
-                    encodeUriComponent(decoded) shouldBe expected
+                ) { (decoded, encoded) ->
+                    withClue("encode") {
+                        encodeUriComponent(decoded) shouldBe encoded
+                    }
+                    withClue("decode") {
+                        decodeUriComponent(encoded) shouldBe decoded
+                    }
                 }
             }
             test("escaped value is correctly parsed by URL") {
@@ -32,6 +39,37 @@ class UriTest : FunSpec(
                     val encoded = encodeUriComponent(query)
 
                     URI("https://ciao.com?$encoded").query shouldBe query
+                }
+            }
+
+            test("round trip") {
+                checkAll(Arb.string()) {
+                    decodeUriComponent(encodeUriComponent(it)) shouldBe it
+                }
+            }
+        }
+
+        context("pathSegments") {
+            context("basic tests") {
+                withData(
+                    nameFn = { "${it.first} -> ${it.second}" },
+                    "schema://authority" to emptyList(),
+                    "schema://authority/" to emptyList(),
+                    "schema://authority/hello/world" to listOf("hello", "world"),
+                    "schema://authority/hello//world" to listOf("hello", "world"),
+                    "schema://authority/hello/world?query#fragment" to listOf("hello", "world"),
+                    "schema://authority/hello%2Fworld" to listOf("hello/world"),
+                    "schema://authority/hello+world" to listOf("hello+world"),
+                    "schema://authority/hello/world?key=val/ue" to listOf("hello", "world"),
+                ) { (uri, segments) ->
+                    URI(uri).pathSegments() shouldBe segments
+                }
+            }
+
+            test("round trip") {
+                checkAll(Arb.list(Arb.string(minSize = 1))) { paths ->
+                    val uri = URI("schema://authority/" + paths.joinToString(separator = "/") { encodeUriComponent(it) })
+                    uri.pathSegments() shouldBe paths
                 }
             }
         }
