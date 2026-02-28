36c7d1fcdaaea643e5615e60f802a5919172048f
Mauro Molin <molin.mauro@gmail.com>
2026-02-28 15:33:08 +0000

    Add ProfileDbActionDialog and use it in DeleteWatchedItemConfirmDialog

--- composeApp/src/main/kotlin/io/github/couchtracker/ui/screens/watchedItem/DeleteWatchedItemConfirmDialog.kt
+++ composeApp/src/main/kotlin/io/github/couchtracker/ui/screens/watchedItem/DeleteWatchedItemConfirmDialog.kt
@@ -1,21 +1,15 @@
 package io.github.couchtracker.ui.screens.watchedItem
 
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
-import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import io.github.couchtracker.R
 import io.github.couchtracker.db.profile.model.watchedItem.WatchedItemWrapper
-import io.github.couchtracker.ui.components.DelayedActionLoadingIndicator
-import io.github.couchtracker.ui.components.ProfileDbErrorDialog
-import io.github.couchtracker.utils.Loadable
-import io.github.couchtracker.utils.rememberProfileDbActionState
+import io.github.couchtracker.ui.components.ProfileDbActionDialog
 import io.github.couchtracker.utils.str
 
 @Composable
@@ -24,39 +18,15 @@ fun DeleteWatchedItemConfirmDialog(
     onDismissRequest: () -> Unit,
     onDeleted: () -> Unit = {},
 ) {
-    val deleteAction = rememberProfileDbActionState<Unit>(
-        onSuccess = {
-            onDismissRequest()
-            onDeleted()
-        },
-        onError = { onDismissRequest() },
+    ProfileDbActionDialog(
+        state = watchedItem,
+        execute = { db, watchedItem -> watchedItem.delete(db) },
+        onDismissRequest = onDismissRequest,
+        onSuccess = { onDeleted() },
+        confirmText = { Text(R.string.delete_action.str()) },
+        confirmButtonColors = ButtonDefaults.textButtonColors(contentColor = MaterialTheme.colorScheme.error),
+        icon = { Icon(Icons.Default.Delete, contentDescription = null) },
+        title = { Text(R.string.delete_viewing_title.str()) },
+        text = { Text(R.string.delete_viewing_description.str()) },
     )
-    if (watchedItem != null) {
-        AlertDialog(
-            onDismissRequest = {
-                // Cant dismiss dialog if it's loading
-                if (deleteAction.current !is Loadable.Loading) {
-                    onDismissRequest()
-                }
-            },
-            icon = { Icon(Icons.Default.Delete, contentDescription = null) },
-            title = { Text(R.string.delete_viewing_title.str()) },
-            text = { Text(R.string.delete_viewing_description.str()) },
-            confirmButton = {
-                TextButton(
-                    enabled = !deleteAction.isLoading,
-                    onClick = { deleteAction.execute { db -> watchedItem.delete(db) } },
-                ) {
-                    DelayedActionLoadingIndicator(action = deleteAction, modifier = Modifier.padding(end = 8.dp))
-                    Text(R.string.delete_action.str())
-                }
-            },
-            dismissButton = {
-                TextButton(onClick = onDismissRequest, enabled = !deleteAction.isLoading) {
-                    Text(android.R.string.cancel.str())
-                }
-            },
-        )
-    }
-    ProfileDbErrorDialog(deleteAction)
 }
