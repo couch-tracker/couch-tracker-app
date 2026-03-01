8845de4f1c624ced3490f11c7c5f878ca8c24f61
Mauro Molin <molin.mauro@gmail.com>
2026-03-01 14:14:47 +0000

    Split PartialDateTime local comparator in high and low precision first
    This includes additional unit testing for those comparators that have now become public.

--- composeApp/src/test/kotlin/io/github/couchtracker/SortTestUtils.kt
+++ composeApp/src/test/kotlin/io/github/couchtracker/SortTestUtils.kt
@@ -1,27 +1,47 @@
 package io.github.couchtracker
 
 import io.kotest.core.spec.style.scopes.FunSpecContainerScope
-import io.kotest.matchers.comparables.shouldBeEqualComparingTo
-import io.kotest.matchers.comparables.shouldBeGreaterThan
-import io.kotest.matchers.comparables.shouldBeLessThan
+import io.kotest.datatest.withContexts
+import io.kotest.datatest.withTests
+import io.kotest.matchers.ints.shouldBeGreaterThan
+import io.kotest.matchers.ints.shouldBeLessThan
+import io.kotest.matchers.shouldBe
 
 object SortTestUtils {
-    fun <T : Comparable<T>> runComparablesTest(items: Iterable<T>) {
-        val list = items.toList()
 
+    fun <T> runComparingTest(items: List<T>, comparator: Comparator<T>) {
         // Check that every item in the list has the expected compareTo() result with every other item in the list (including itself)
-        for ((ia, a) in list.withIndex()) {
-            for (b in list.subList(fromIndex = ia + 1, toIndex = list.size)) {
-                a shouldBeLessThan b
-                b shouldBeGreaterThan a
+        for ((ia, a) in items.withIndex()) {
+            for (b in items.subList(fromIndex = ia + 1, toIndex = items.size)) {
+                comparator.compare(a, b) shouldBeLessThan 0
+                comparator.compare(b, a) shouldBeGreaterThan 0
             }
-            a shouldBeEqualComparingTo a
+            comparator.compare(a, a) shouldBe 0
         }
     }
 }
 
 suspend fun <T : Comparable<T>> FunSpecContainerScope.testComparables(name: String, vararg items: T) {
+    testComparator(
+        name = name,
+        comparator = Comparator.comparing { it },
+        items = items,
+    )
+}
+
+suspend fun <T : Comparable<T>> FunSpecContainerScope.testComparator(name: String, comparator: Comparator<in T>, vararg items: T) {
     test(name) {
-        SortTestUtils.runComparablesTest(items.toList())
+        SortTestUtils.runComparingTest(items.toList(), comparator)
+    }
+}
+
+suspend fun <T> FunSpecContainerScope.testComparators(
+    comparators: Map<String, Comparator<in T>>,
+    testCases: Map<String, List<T>>,
+) {
+    withContexts(comparators) { comparator ->
+        withTests(testCases) { items ->
+            SortTestUtils.runComparingTest(items, comparator)
+        }
     }
 }
