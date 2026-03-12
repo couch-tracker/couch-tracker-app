396b66373f51982669754926540d91da0a38e008
Mauro Molin <molin.mauro@gmail.com>
2026-03-12 02:08:01 +0000

    Add RelativeLocalDateFormatter

---
+++ composeApp/src/main/kotlin/io/github/couchtracker/intl/datetime/RelativeLocalDateFormatter.kt
@@ -0,0 +1,93 @@
+package io.github.couchtracker.intl.datetime
+
+import com.ibm.icu.text.DisplayContext
+import com.ibm.icu.text.NumberFormat
+import com.ibm.icu.text.RelativeDateTimeFormatter
+import com.ibm.icu.text.RelativeDateTimeFormatter.AbsoluteUnit
+import com.ibm.icu.text.RelativeDateTimeFormatter.Direction
+import com.ibm.icu.text.RelativeDateTimeFormatter.RelativeUnit
+import com.ibm.icu.util.Calendar
+import com.ibm.icu.util.ULocale
+import io.github.couchtracker.utils.TickingValue
+import kotlinx.datetime.DatePeriod
+import kotlinx.datetime.DayOfWeek
+import kotlinx.datetime.LocalDate
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.atStartOfDayIn
+import kotlinx.datetime.daysUntil
+import kotlinx.datetime.plus
+import kotlinx.datetime.toLocalDateTime
+import kotlin.math.absoluteValue
+import kotlin.time.Instant
+
+private const val TWO_DAYS_AGO = -2
+private const val ONE_DAY_AGO = -1
+private const val TODAY = 0
+private const val IN_ONE_DAY = 1
+private const val IN_TWO_DAYS = 2
+
+data class RelativeLocalDateFormatter(
+    private val locale: ULocale,
+    private val numberFormat: NumberFormat? = null,
+    private val style: RelativeDateTimeFormatter.Style = RelativeDateTimeFormatter.Style.LONG,
+    private val capitalizationContext: DisplayContext = DisplayContext.CAPITALIZATION_NONE,
+) {
+
+    private val relativeDateTimeFormatter = RelativeDateTimeFormatter.getInstance(locale, numberFormat, style, capitalizationContext)
+
+    fun format(date: LocalDate, now: Instant, tz: TimeZone): TickingValue<String> {
+        val nowInDate = now.toLocalDateTime(tz).date
+        val days = nowInDate.daysUntil(date)
+
+        val absoluteFormattedDay = when (days) {
+            in TWO_DAYS_AGO..IN_TWO_DAYS -> {
+                val direction = when (days) {
+                    TWO_DAYS_AGO -> Direction.LAST_2
+                    ONE_DAY_AGO -> Direction.LAST
+                    TODAY -> Direction.THIS
+                    IN_ONE_DAY -> Direction.NEXT
+                    IN_TWO_DAYS -> Direction.NEXT_2
+                    else -> error("invalid days amount $days")
+                }
+                // This returns null if the locale doesn't have a way to say this
+                relativeDateTimeFormatter.format(direction, AbsoluteUnit.DAY)
+            }
+            else -> null
+        }
+
+        val thisNextDow = if (absoluteFormattedDay == null && days in 1..DayOfWeek.entries.size) {
+            val firstDoWIndex = Calendar.getInstance(locale).firstDayOfWeek
+            val firstDoW = DayOfWeek.entries.single {
+                it.ordinal == (firstDoWIndex - 2).mod(DayOfWeek.entries.size)
+            }
+            val nextFirstDoW = List(DayOfWeek.entries.size) { nowInDate.plus(DatePeriod(days = it + 1)) }.first { it.dayOfWeek == firstDoW }
+
+            val direction = when {
+                date < nextFirstDoW -> Direction.THIS
+                else -> Direction.NEXT
+            }
+            relativeDateTimeFormatter.format(direction, date.dayOfWeek.toIcuAbsoluteUnit())
+        } else {
+            null
+        }
+
+        return TickingValue(
+            value = absoluteFormattedDay ?: thisNextDow ?: relativeDateTimeFormatter.format(
+                days.absoluteValue.toDouble(),
+                if (days < 0) Direction.LAST else Direction.NEXT,
+                RelativeUnit.DAYS,
+            ),
+            nextTick = (nowInDate + DatePeriod(days = 1)).atStartOfDayIn(tz) - now,
+        )
+    }
+}
+
+private fun DayOfWeek.toIcuAbsoluteUnit(): AbsoluteUnit = when (this) {
+    DayOfWeek.MONDAY -> AbsoluteUnit.MONDAY
+    DayOfWeek.TUESDAY -> AbsoluteUnit.TUESDAY
+    DayOfWeek.WEDNESDAY -> AbsoluteUnit.WEDNESDAY
+    DayOfWeek.THURSDAY -> AbsoluteUnit.THURSDAY
+    DayOfWeek.FRIDAY -> AbsoluteUnit.FRIDAY
+    DayOfWeek.SATURDAY -> AbsoluteUnit.SATURDAY
+    DayOfWeek.SUNDAY -> AbsoluteUnit.SUNDAY
+}
---
+++ composeApp/src/test/kotlin/io/github/couchtracker/intl/datetime/RelativeLocalDateFormatterTest.kt
@@ -0,0 +1,214 @@
+package io.github.couchtracker.intl.datetime
+
+import com.ibm.icu.text.DisplayContext
+import com.ibm.icu.text.RelativeDateTimeFormatter
+import com.ibm.icu.util.ULocale
+import io.github.couchtracker.utils.TickingValue
+import io.kotest.assertions.withClue
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.core.tuple
+import io.kotest.datatest.withContexts
+import io.kotest.datatest.withTests
+import io.kotest.matchers.nulls.shouldNotBeNull
+import io.kotest.matchers.should
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.shouldNotBe
+import io.kotest.matchers.string.shouldContain
+import io.kotest.property.Arb
+import io.kotest.property.arbitrary.kotlinInstant
+import io.kotest.property.arbitrary.localDate
+import io.kotest.property.arbitrary.map
+import io.kotest.property.arbitrary.zoneId
+import io.kotest.property.checkAll
+import kotlinx.datetime.DateTimeUnit
+import kotlinx.datetime.LocalDate
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.atStartOfDayIn
+import kotlinx.datetime.plus
+import kotlinx.datetime.toKotlinLocalDate
+import kotlinx.datetime.toKotlinTimeZone
+import kotlin.time.Duration.Companion.hours
+import kotlin.time.Duration.Companion.minutes
+import kotlin.time.Duration.Companion.nanoseconds
+import kotlin.time.Duration.Companion.seconds
+import kotlin.time.Instant
+
+/**
+ * Current date used in most tests. This is a Wednesday
+ */
+private val CURRENT_DATE = LocalDate.parse("2025-12-31")
+
+class RelativeLocalDateFormatterTest : FunSpec(
+    {
+        context("format") {
+            context("formatted string") {
+                context("number of days") {
+                    withTests(
+                        nameFn = { it.toString() },
+                        tuple(ULocale.ENGLISH, -124, "124 days ago"),
+                        tuple(ULocale.ENGLISH, -10, "10 days ago"),
+                        tuple(ULocale.ENGLISH, -2, "2 days ago"),
+                        tuple(ULocale.ENGLISH, 15, "in 15 days"),
+                        tuple(ULocale.ENGLISH, 555, "in 555 days"),
+                        tuple(ULocale.ITALIAN, -124, "124 giorni fa"),
+                        tuple(ULocale.ITALIAN, -10, "10 giorni fa"),
+                        tuple(ULocale.ITALIAN, -3, "3 giorni fa"),
+                        tuple(ULocale.ITALIAN, 15, "tra 15 giorni"),
+                        tuple(ULocale.ITALIAN, 555, "tra 555 giorni"),
+                    ) { (locale, days, expected) ->
+                        RelativeLocalDateFormatter(locale).format(days).value shouldBe expected
+                    }
+                }
+
+                context("single word") {
+                    withTests(
+                        nameFn = { it.toString() },
+                        tuple(ULocale.ENGLISH, -1, "yesterday"),
+                        tuple(ULocale.ENGLISH, 0, "today"),
+                        tuple(ULocale.ENGLISH, 1, "tomorrow"),
+                        tuple(ULocale.ITALIAN, -2, "l’altro ieri"),
+                        tuple(ULocale.ITALIAN, 2, "dopodomani"),
+                    ) { (locale, days, expected) ->
+                        RelativeLocalDateFormatter(locale).format(days).value shouldBe expected
+                    }
+                }
+
+                context("up to 7 days in the future uses this/next day of week") {
+                    withTests(
+                        nameFn = { it.toString() },
+                        tuple(ULocale.ENGLISH, 2, "this Friday"),
+                        tuple(ULocale.ENGLISH, 3, "this Saturday"),
+                        // English's first day of week is Sunday
+                        tuple(ULocale.ENGLISH, 4, "next Sunday"),
+                        tuple(ULocale.ENGLISH, 7, "next Wednesday"),
+                        tuple(ULocale.ENGLISH, 8, "in 8 days"),
+
+                        tuple(ULocale.ITALIAN, 3, "questo sabato"),
+                        tuple(ULocale.ITALIAN, 4, "questa domenica"),
+                        // Italian's first day of week is Monday
+                        tuple(ULocale.ITALIAN, 5, "lunedì prossimo"),
+                        tuple(ULocale.ITALIAN, 7, "mercoledì prossimo"),
+                    ) { (locale, days, expectedFormat) ->
+                        RelativeLocalDateFormatter(locale).format(days).value shouldBe expectedFormat
+                    }
+                }
+
+                context("number format follows locale convention") {
+                    withTests(
+                        nameFn = { it.first.toString() },
+                        ULocale.ENGLISH to "1,234,567",
+                        ULocale.ITALIAN to "1.234.567",
+                        ULocale("hi") to "12,34,567",
+                    ) { (locale, expected) ->
+                        RelativeLocalDateFormatter(locale).format(-1_234_567).value shouldContain expected
+                    }
+                }
+
+                context("different styles are respected") {
+                    withContexts(
+                        nameFn = { it.toString() },
+                        tuple(RelativeDateTimeFormatter.Style.LONG, -1, "yesterday"),
+                        tuple(RelativeDateTimeFormatter.Style.LONG, 2, "this Friday"),
+                        tuple(RelativeDateTimeFormatter.Style.LONG, 10, "in 10 days"),
+                        tuple(RelativeDateTimeFormatter.Style.SHORT, -1, "yesterday"),
+                        tuple(RelativeDateTimeFormatter.Style.SHORT, 2, "this Fri."),
+                        tuple(RelativeDateTimeFormatter.Style.SHORT, 10, "in 10 days"),
+                        tuple(RelativeDateTimeFormatter.Style.NARROW, -1, "yesterday"),
+                        tuple(RelativeDateTimeFormatter.Style.NARROW, 2, "this F"),
+                        tuple(RelativeDateTimeFormatter.Style.NARROW, 10, "in 10d"),
+                    ) { (style, days, expected) ->
+                        val formatter = RelativeLocalDateFormatter(locale = ULocale.ENGLISH, style = style)
+                        formatter.format(days).value shouldBe expected
+                    }
+                }
+                context("different capitalizations are respected") {
+                    withContexts(
+                        nameFn = { it.toString() },
+                        tuple(DisplayContext.CAPITALIZATION_NONE, 0, "today"),
+                        tuple(DisplayContext.CAPITALIZATION_NONE, 2, "this Friday"),
+                        tuple(DisplayContext.CAPITALIZATION_NONE, 10, "in 10 days"),
+                        tuple(DisplayContext.CAPITALIZATION_FOR_BEGINNING_OF_SENTENCE, 0, "Today"),
+                        tuple(DisplayContext.CAPITALIZATION_FOR_BEGINNING_OF_SENTENCE, 2, "This Friday"),
+                        tuple(DisplayContext.CAPITALIZATION_FOR_BEGINNING_OF_SENTENCE, 10, "In 10 days"),
+                    ) { (capitalizationContext, days, expected) ->
+                        val formatter = RelativeLocalDateFormatter(locale = ULocale.ENGLISH, capitalizationContext = capitalizationContext)
+                        formatter.format(days).value shouldBe expected
+                    }
+                }
+            }
+
+            context("next tick") {
+                val formatter = RelativeLocalDateFormatter(ULocale.ENGLISH)
+
+                context("basic tests") {
+                    val date = LocalDate.parse("2026-01-01")
+                    withTests(
+                        nameFn = { "${it.a} ${it.b}" },
+                        tuple(
+                            Instant.parse("2026-01-01T21:00:00Z"),
+                            TimeZone.UTC,
+                            3.hours,
+                        ),
+                        tuple(
+                            Instant.parse("2026-01-01T23:59:59Z"),
+                            TimeZone.UTC,
+                            1.seconds,
+                        ),
+                        tuple(
+                            Instant.parse("2026-01-01T22:35:41Z"),
+                            TimeZone.of("Europe/Berlin"),
+                            24.minutes + 19.seconds,
+                        ),
+                    ) { (now, tz, expected) ->
+                        formatter.format(date, now, tz).nextTick shouldBe expected
+                    }
+                }
+
+                context("handles DST changes") {
+                    val tz = TimeZone.of("Europe/Rome")
+                    test("jump forward") {
+                        val date = LocalDate.parse("2025-03-30")
+                        formatter.format(date = date, now = date.atStartOfDayIn(tz), tz = tz).nextTick shouldBe 23.hours
+                    }
+                    test("just backward") {
+                        val date = LocalDate.parse("2025-10-26")
+                        formatter.format(date = date, now = date.atStartOfDayIn(tz), tz = tz).nextTick shouldBe 25.hours
+                    }
+                }
+                test("correctly predicts date change") {
+                    checkAll(
+                        iterations = 1000,
+                        Arb.localDate().map { it.toKotlinLocalDate() },
+                        Arb.kotlinInstant(),
+                        Arb.zoneId().map { it.toKotlinTimeZone() },
+                    ) { date, now, tz ->
+                        val formatted = formatter.format(date, now, tz)
+
+                        val nextTick = withClue("next tick should never be null") {
+                            formatted.nextTick.shouldNotBeNull()
+                        }
+
+                        withClue("format at 1 nanosecond before nextTick should yield same relative value") {
+                            formatter.format(date, now + nextTick - 1.nanoseconds, tz) should {
+                                it.value shouldBe formatted.value
+                                it.nextTick shouldBe 1.nanoseconds
+                            }
+                        }
+
+                        withClue("format at nextTick should yield different relative value") {
+                            formatter.format(date, now + nextTick, tz) should {
+                                it.value shouldNotBe formatted.value
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    },
+)
+
+private fun RelativeLocalDateFormatter.format(days: Int): TickingValue<String> {
+    val tz = TimeZone.UTC
+    val now = CURRENT_DATE.atStartOfDayIn(tz)
+    return format(CURRENT_DATE.plus(days, DateTimeUnit.DAY), now, tz)
+}
