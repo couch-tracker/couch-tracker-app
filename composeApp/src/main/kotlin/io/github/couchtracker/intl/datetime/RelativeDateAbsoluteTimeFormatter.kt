65871fa5641a1c2050ed85d2ecd2fe12e016f249
Mauro Molin <molin.mauro@gmail.com>
2026-03-14 16:42:56 +0000

    Add RelativeDateAbsoluteTimeFormatter

---
+++ composeApp/src/main/kotlin/io/github/couchtracker/intl/datetime/RelativeDateAbsoluteTimeFormatter.kt
@@ -0,0 +1,55 @@
+package io.github.couchtracker.intl.datetime
+
+import com.ibm.icu.impl.SimpleFormatterImpl
+import com.ibm.icu.text.DateFormat
+import com.ibm.icu.text.DateTimePatternGenerator
+import com.ibm.icu.text.RelativeDateTimeFormatter
+import com.ibm.icu.text.SimpleDateFormat
+import com.ibm.icu.util.ULocale
+import io.github.couchtracker.utils.TickingValue
+import kotlinx.datetime.LocalDateTime
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.toJavaLocalDateTime
+import kotlinx.datetime.toJavaZoneId
+import kotlin.time.Instant
+
+/**
+ * Class that formats the date in a relative way (see [RelativeLocalDateFormatter]), while the time always formatted in an absolute way.
+ *
+ * Examples:
+ * - `today at 10:30`
+ * - `this Monday at 6pm`
+ * - `5 days ago at 4:21`
+ */
+class RelativeDateAbsoluteTimeFormatter(
+    private val locale: ULocale,
+    private val timeSkeleton: TimeSkeleton = TimeSkeleton.MINUTES,
+    relativeStyle: RelativeDateTimeFormatter.Style = RelativeDateTimeFormatter.Style.LONG,
+    private val dateFormatStyle: Int = DateFormat.FULL,
+) {
+
+    init {
+        require(dateFormatStyle in DateFormat.FULL..DateFormat.SHORT) { "Invalid date format style: $dateFormatStyle" }
+    }
+
+    private val relativeLocalDateFormatter = RelativeLocalDateFormatter(locale, style = relativeStyle)
+
+    fun format(dateTime: LocalDateTime, now: Instant, tz: TimeZone): TickingValue<String> {
+        val dateFormat = relativeLocalDateFormatter.format(dateTime.date, now, tz)
+
+        val generator = DateTimePatternGenerator.getInstance(locale)
+        val dateTimePattern = generator.getDateTimeFormat(dateFormatStyle)
+        val pattern = SimpleFormatterImpl.formatRawPattern(
+            dateTimePattern,
+            2, // min
+            2, // max
+            generator.getBestPattern(timeSkeleton.value),
+            "'${dateFormat.value.replace("'", "''")}'",
+        )
+
+        return TickingValue(
+            value = SimpleDateFormat(pattern, locale).format(dateTime.toJavaLocalDateTime().atZone(tz.toJavaZoneId())),
+            nextTick = dateFormat.nextTick,
+        )
+    }
+}
---
+++ composeApp/src/test/kotlin/io/github/couchtracker/intl/datetime/RelativeDateAbsoluteTimeFormatterTest.kt
@@ -0,0 +1,137 @@
+package io.github.couchtracker.intl.datetime
+
+import com.ibm.icu.text.DateFormat
+import com.ibm.icu.text.RelativeDateTimeFormatter
+import com.ibm.icu.util.ULocale
+import io.github.couchtracker.utils.LocaleData
+import io.github.couchtracker.utils.TickingValue
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.core.tuple
+import io.kotest.datatest.withTests
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.string.shouldContain
+import io.kotest.property.Arb
+import io.kotest.property.arbitrary.enum
+import io.kotest.property.arbitrary.kotlinInstant
+import io.kotest.property.arbitrary.localDateTime
+import io.kotest.property.arbitrary.map
+import io.kotest.property.arbitrary.zoneId
+import io.kotest.property.checkAll
+import io.kotest.property.exhaustive.exhaustive
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.toKotlinLocalDateTime
+import kotlinx.datetime.toKotlinTimeZone
+import kotlinx.datetime.toLocalDateTime
+import kotlin.time.Duration
+import kotlin.time.Duration.Companion.days
+import kotlin.time.Duration.Companion.hours
+import kotlin.time.Duration.Companion.minutes
+import kotlin.time.Duration.Companion.seconds
+import kotlin.time.Instant
+
+/**
+ * Current date used in most tests. This is a Thursday
+ */
+private val NOW = Instant.parse("2026-01-01T00:00:00Z")
+private val TZ = TimeZone.UTC
+
+class RelativeDateAbsoluteTimeFormatterTest : FunSpec(
+    {
+        context("format") {
+            context("formatted value") {
+                context("works") {
+                    withTests(
+                        nameFn = { Pair(it.b, it.c).toString() },
+                        tuple(
+                            RelativeDateAbsoluteTimeFormatter(ULocale.ENGLISH),
+                            15.days + 3.hours,
+                            "in 15 days at 3:00 AM",
+                        ),
+                        tuple(
+                            RelativeDateAbsoluteTimeFormatter(ULocale.ENGLISH),
+                            -3.hours,
+                            "yesterday at 9:00 PM",
+                        ),
+                        tuple(
+                            RelativeDateAbsoluteTimeFormatter(ULocale.ENGLISH),
+                            -(20.days + 5.hours),
+                            "21 days ago at 7:00 PM",
+                        ),
+                        tuple(
+                            RelativeDateAbsoluteTimeFormatter(ULocale.ENGLISH, timeSkeleton = TimeSkeleton.SECONDS),
+                            4.hours + 13.minutes + 5.seconds,
+                            "today at 4:13:05 AM",
+                        ),
+                        tuple(
+                            RelativeDateAbsoluteTimeFormatter(ULocale.ENGLISH),
+                            3.days + 4.hours,
+                            "next Sunday at 4:00 AM",
+                        ),
+                        tuple(
+                            RelativeDateAbsoluteTimeFormatter(ULocale.ENGLISH, relativeStyle = RelativeDateTimeFormatter.Style.SHORT),
+                            3.days + 4.hours,
+                            "next Sun. at 4:00 AM",
+                        ),
+                        tuple(
+                            RelativeDateAbsoluteTimeFormatter(ULocale.ENGLISH, dateFormatStyle = DateFormat.SHORT),
+                            3.days + 4.hours,
+                            "next Sunday, 4:00 AM",
+                        ),
+                        tuple(
+                            RelativeDateAbsoluteTimeFormatter(ULocale.ENGLISH, dateFormatStyle = DateFormat.LONG),
+                            3.days + 4.hours,
+                            "next Sunday at 4:00 AM",
+                        ),
+                        tuple(
+                            RelativeDateAbsoluteTimeFormatter(ULocale.ITALIAN),
+                            -3.hours,
+                            "ieri alle ore 21:00",
+                        ),
+                        tuple(
+                            RelativeDateAbsoluteTimeFormatter(ULocale.ITALIAN, dateFormatStyle = DateFormat.LONG),
+                            5.days + 15.hours,
+                            "martedì prossimo alle ore 15:00",
+                        ),
+                    ) { (formatter, diff, expected) ->
+                        formatter.format(diff).value shouldBe expected
+                    }
+                }
+
+                test("must contain relative formatted local date") {
+                    checkAll(
+                        LocaleData().allLocales.exhaustive(),
+                        Arb.localDateTime().map { it.toKotlinLocalDateTime() },
+                        Arb.kotlinInstant(),
+                        Arb.zoneId().map { it.toKotlinTimeZone() },
+                        Arb.enum<RelativeDateTimeFormatter.Style>(),
+                    ) { locale, dateTime, now, tz, relativeStyle ->
+                        val dateString = RelativeLocalDateFormatter(locale, style = relativeStyle).format(dateTime.date, now, tz).value
+                        val formatted =
+                            RelativeDateAbsoluteTimeFormatter(locale, relativeStyle = relativeStyle).format(dateTime, now, tz).value
+                        formatted shouldContain dateString
+                    }
+                }
+            }
+
+            test("nextTick is always identical to RelativeLocalDateFormatter (as time is always absolute)") {
+                val formatter = RelativeDateAbsoluteTimeFormatter(ULocale.ENGLISH)
+                val dateFormatter = RelativeLocalDateFormatter(ULocale.ENGLISH)
+                checkAll(
+                    Arb.localDateTime().map { it.toKotlinLocalDateTime() },
+                    Arb.kotlinInstant(),
+                    Arb.zoneId().map { it.toKotlinTimeZone() },
+                ) { localDateTime, now, zoneId ->
+                    formatter.format(localDateTime, now, zoneId).nextTick shouldBe dateFormatter.format(
+                        localDateTime.date,
+                        now,
+                        zoneId,
+                    ).nextTick
+                }
+            }
+        }
+    },
+)
+
+private fun RelativeDateAbsoluteTimeFormatter.format(diff: Duration): TickingValue<String> {
+    return format((NOW + diff).toLocalDateTime(TZ), NOW, TZ)
+}
