dc1266857b780bde513d71d0df43855607422320
Marco <802019+MMarco94@users.noreply.github.com>
2026-04-21 23:10:36 +0100

    Bugfix when rating is negative

--- composeApp/src/test/kotlin/io/github/couchtracker/tmdb/TmdbRatingTest.kt
+++ composeApp/src/test/kotlin/io/github/couchtracker/tmdb/TmdbRatingTest.kt
@@ -5,6 +5,11 @@ import io.kotest.datatest.withData
 import io.kotest.matchers.nulls.shouldBeNull
 import io.kotest.matchers.nulls.shouldNotBeNull
 import io.kotest.matchers.shouldBe
+import io.kotest.property.Arb
+import io.kotest.property.arbitrary.float
+import io.kotest.property.arbitrary.int
+import io.kotest.property.arbitrary.orNull
+import io.kotest.property.checkAll
 
 class TmdbRatingTest : FunSpec(
     {
@@ -19,8 +24,11 @@ class TmdbRatingTest : FunSpec(
                 TestCase(avg = 3f, count = 2, expectedAvg = 3f, expectedCount = 2),
                 TestCase(avg = 3f, count = null, expectedAvg = 3f, expectedCount = null),
                 TestCase(avg = 0f, count = null, expectedAvg = null, expectedCount = null),
+                TestCase(avg = 0f, count = -1, expectedAvg = null, expectedCount = null),
+                TestCase(avg = 0f, count = 0, expectedAvg = null, expectedCount = null),
+                TestCase(avg = 0f, count = 10, expectedAvg = 0f, expectedCount = 10),
                 TestCase(avg = -3f, count = 10, expectedAvg = null, expectedCount = null),
-                TestCase(avg = 3f, count = 0, expectedAvg = null, expectedCount = null),
+                TestCase(avg = 3f, count = 0, expectedAvg = 3f, expectedCount = null),
                 TestCase(avg = null, count = 0, expectedAvg = null, expectedCount = null),
                 TestCase(avg = null, count = 10, expectedAvg = null, expectedCount = null),
                 TestCase(avg = null, count = null, expectedAvg = null, expectedCount = null),
@@ -32,6 +40,21 @@ class TmdbRatingTest : FunSpec(
                     parsed.shouldNotBeNull()
                     parsed.average shouldBe testCase.expectedAvg
                     parsed.count shouldBe testCase.expectedCount
+                }
+            }
+            test("fuzz") {
+                checkAll(Arb.float().orNull(), Arb.int().orNull()) { avg, count ->
+                    // Doesn't throw
+                    val rating = TmdbRating.ofOrNull(avg, count)
+                    if (rating?.average != null) {
+                        rating.average shouldBe avg
+                    }
+                    if (rating?.count != null) {
+                        rating.count shouldBe count
+                    }
+                    if (avg != null && avg > 0f) {
+                        rating?.average shouldBe avg
+                    }
                 }
             }
         }
