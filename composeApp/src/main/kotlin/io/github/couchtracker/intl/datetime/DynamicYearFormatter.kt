28d6093de233f39fead83e835197c180993894f7
Mauro Molin <molin.mauro@gmail.com>
2026-05-12 23:54:46 +0100

    Add DynamicYearFormatter

---
+++ composeApp/src/main/kotlin/io/github/couchtracker/intl/datetime/DynamicYearFormatter.kt
@@ -0,0 +1,76 @@
+package io.github.couchtracker.intl.datetime
+
+import com.ibm.icu.text.DateFormat
+import com.ibm.icu.text.RelativeDateTimeFormatter
+import com.ibm.icu.util.ULocale
+import io.github.couchtracker.intl.combineLiteralDateAndTimePattern
+import io.github.couchtracker.intl.formatDateTimeSkeleton
+import io.github.couchtracker.utils.MaybeZoned
+import io.github.couchtracker.utils.TickingValue
+import io.github.couchtracker.utils.Zoned
+import io.github.couchtracker.utils.map
+import kotlinx.datetime.LocalDate
+import kotlinx.datetime.Month
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.atStartOfDayIn
+import kotlinx.datetime.toJavaZoneId
+import java.time.ZonedDateTime
+import kotlin.time.Duration.Companion.nanoseconds
+import kotlin.time.Instant
+import kotlin.time.toJavaInstant
+
+class DynamicYearFormatter(
+    private val locale: ULocale,
+    private val timeZoneSkeleton: TimezoneSkeleton = TimezoneSkeleton.TIMEZONE_ID,
+    relativeDateStyle: RelativeDateTimeFormatter.Style = RelativeDateTimeFormatter.Style.LONG,
+    val absoluteSkeleton: YearSkeleton = YearSkeleton.NUMERIC,
+) {
+
+    private val relativeYearFormatter = RelativeYearFormatter(
+        locale = locale,
+        style = relativeDateStyle,
+    )
+
+    fun format(year: MaybeZoned<Int>, now: Zoned<Instant>): TickingValue<String> {
+        return chooseThreshold(
+            now = now.value,
+            thresholdStart = LocalDate(year = year.value - 1, month = Month.JANUARY, day = 1).atStartOfDayIn(now.timeZone) - 1.nanoseconds,
+            thresholdEnd = LocalDate(year = year.value + 2, month = Month.JANUARY, day = 1).atStartOfDayIn(now.timeZone),
+            withinThreshold = {
+                val relativeYearFormat = relativeYearFormatter.format(year.value, now)
+
+                if (year.timeZone != null) {
+                    relativeYearFormat.map {
+                        combineLiteralDateAndTimePattern(
+                            locale = locale,
+                            dateFormatStyle = DateFormat.SHORT,
+                            literalDate = it,
+                            timePattern = { timeZoneSkeleton.value },
+                            value = ZonedDateTime.ofInstant(
+                                LocalDate(year.value, Month.JANUARY, 1).atStartOfDayIn(year.timeZone).toJavaInstant(),
+                                year.timeZone.toJavaZoneId(),
+                            ),
+                        )
+                    }
+                } else {
+                    relativeYearFormat
+                }
+            },
+            outsideThreshold = {
+                val tz = year.timeZone ?: TimeZone.UTC
+                TickingValue(
+                    value = formatDateTimeSkeleton(
+                        instant = LocalDate(year.value, month = Month.JANUARY, day = 1).atStartOfDayIn(tz),
+                        timeZone = tz,
+                        skeleton = listOfNotNull(
+                            absoluteSkeleton,
+                            if (year.timeZone != null) timeZoneSkeleton else null,
+                        ).sum(),
+                        locale = locale,
+                    ),
+                    nextTick = null,
+                )
+            },
+        )
+    }
+}
